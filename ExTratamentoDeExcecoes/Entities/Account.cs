9eda988a490692ed2f3e00a507f37c4e4a344964
Mateus <devmateussnr@gmail.com>
2023-05-06 18:41:56 -0300

    Update
    Code package.

---
+++ ExTratamentoDeExcecoes/Entities/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ExTratamentoDeExcecoes.Entities.Exceptions;
+
+namespace ExTratamentoDeExcecoes.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double withdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+        
+        public Account(int number, string holder, double balance, double withdrawlimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            withdrawLimit = withdrawlimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public double withdraw(double amount)
+        {
+            if(Balance < amount)
+            {
+                throw new DomainException("Erro: Saldo insuficiente para saque.");
+            }
+            else if(amount > withdrawLimit)
+            {
+                throw new DomainException("Erro: Limite de saque não autorizado.");
+            }
+            else
+            {
+                return Balance-= amount;
+            }
+        }
+    }
+}
---
+++ ExTratamentoDeExcecoes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ExTratamentoDeExcecoes.Entities;
+using System.Globalization;
+using ExTratamentoDeExcecoes.Entities.Exceptions;
+
+namespace ExTratamentoDeExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+
+                Console.Write("Number: ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double withdrawlimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(n, holder, balance,withdrawlimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("New balance: " + account.withdraw(amount).ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)// Tratamento para exceções exclusivas
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)// Tratamento para exceções gerais
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
